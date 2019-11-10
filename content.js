f43c1ed1a6eead0875bf5270de3a14605b0fdd4e
Andrew <betelgeuse.as@gmail.com>
2019-11-10 12:40:10 +0200

    content logic

--- content.js
+++ content.js
@@ -1,1 +1,30 @@
-console.log(true);
+const listen = (selector = '', event = '', listener = () => {}, useCapture = false) => {
+  document.querySelector(selector)
+    .addEventListener(event, listener, useCapture);
+};
+
+// listen('body', 'mouseup', (e) => {
+//   // let selection = document.getSelection ? document.getSelection().toString() :  document.selection.createRange().toString() ;
+//   console.log(document.getSelection().toString());
+//
+//   // console.log('Selection object:', window.getSelection());
+//   // console.log('Selected text:', window.getSelection().toString());
+// });
+
+
+listen('body', 'keydown', (e) => {
+  let eventObj = window.event ? event : e;
+
+  switch(true) {
+    case (eventObj.keyCode === 83 && eventObj.ctrlKey && eventObj.shiftKey)://Ctrl + Shift + S
+      console.log(document.getSelection().toString());
+      break;
+
+    case (eventObj.keyCode === 65 && eventObj.ctrlKey && eventObj.shiftKey)://Ctrl + Shift + A
+      console.log(document.getSelection().toString());
+      break;
+
+    default:
+      break;
+  }
+});
