b156d31cce7413f4821c67ac7740792ec05605de
Andrew <betelgeuse.as@gmail.com>
2019-11-11 22:50:44 +0200

    api wikipedia

--- popup/popup.js
+++ popup/popup.js
@@ -31,11 +31,10 @@ for(let i = 0; i < events.length; i++) {
           break;
 
         case 'keydown':
-            document.body.classList.add('is-type');
-            if ((e.which === 8) && this.value === '') {
-              document.body.classList.remove('is-type');
-            }
-
+          document.body.classList.add('is-type');
+          // if ((e.which === 8) && this.value === '') {
+          //   document.body.classList.remove('is-type');
+          // }
           break;
 
         default:
@@ -44,3 +43,37 @@ for(let i = 0; i < events.length; i++) {
     };
   })();
 }
+
+listen('.field', 'keypress', debounce(function(e) {
+  //TODO set search language
+  let url = "https://en.wikipedia.org/w/api.php";
+
+  let params = {
+    action: "opensearch",
+    prop: "info",
+    limit: "5",
+    search: this.value,
+    namespace: "0",
+    format: "json",
+
+    //A cookie associated with a cross-site resource:
+    //https://www.chromestatus.com/feature/5088147346030592
+    //https://www.chromestatus.com/feature/5633521622188032
+    SameSite: 'None'
+  };
+
+  url = url + "?origin=*";
+  Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+  fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {
+      document.body.classList.remove('is-type');
+      //response?.query?.search[0];
+      let titles = response[1];
+      let description = response[2];
+      let links = response[3];
+      console.log(response);
+    })
+    .catch(function(error){console.log(error);});
+}, 2000));
