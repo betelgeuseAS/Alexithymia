fe784dde58fa8ff8fb15b89db39aeb908bb71428
Andrew <betelgeuse.as@gmail.com>
2019-11-11 22:51:20 +0200

    debounce

--- helper/helper.js
+++ helper/helper.js
@@ -3,3 +3,15 @@ const listen = (selector = '', event = '', listener = () => {}, useCapture = fal
   document.querySelector(selector)
     .addEventListener(event, listener, useCapture);
 };
+
+//Debounce
+//Make sure that the function returned is not an arrow function, as you will lose context.
+const debounce = (fn, time) => {
+  let timeout;
+
+  return function() {
+    const functionCall = () => fn.apply(this, arguments);
+    clearTimeout(timeout);
+    timeout = setTimeout(functionCall, time);
+  }
+};
