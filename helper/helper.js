a59e33ad2bbe0fde253eb402527f35b311ecfb0e
Andrew <betelgeuse.as@gmail.com>
2019-11-15 22:02:29 +0200

    create elemtn method

--- helper/helper.js
+++ helper/helper.js
@@ -1,5 +1,5 @@
 //Customize event listener
-const listen = (selector = '', event = '', listener = () => {}, useCapture = false) => {
+const listen = (selector = 'body', event = '', listener = () => {}, useCapture = false) => {
   document.querySelector(selector)
     .addEventListener(event, listener, useCapture);
 };
@@ -19,5 +19,24 @@ const debounce = (fn, time) => {
 //Set browser language
 const browserLanguage = () => {
   let userLang = navigator.language || navigator.userLanguage;
+
   return userLang ? userLang.slice(0, 2) : 'en';
 };
+
+//Create element node
+function createElement(tag, props, ...children) {
+  const element = document.createElement(tag);
+
+  Object.keys(props).forEach(key => element[key] = props[key]);
+
+  if (children.length > 0) {
+    children.forEach(child => {
+      if (typeof child === 'string') {
+        child = document.createTextNode(child);
+      }
+      element.appendChild(child);
+    });
+  }
+
+  return element;
+}
